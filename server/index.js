bb2ba6d4cf6670dd7a3b98052481e5d5edbe3870
SAMIE228 <samleclaude1@gmail.com>
2022-08-03 14:59:26 +0000

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require ('body-parser')
+const cors = require ('cors')
+const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "CRUDDataBase",
+});
+
+app.use(cors());
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.post("/api/insert", (req, res) => {
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+    const sqlInsert=
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(err);
+    });
+});  
+
+app.listen(3001, () => {
+    console.log("runing on port 3001")
+});
